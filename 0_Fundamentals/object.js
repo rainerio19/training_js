940373da5d3258b51f4e8a5062fb97f0c9b2882a
rainerio <rai.inerio@hotmail.com>
2020-12-03 22:31:33 -0300

    objects

---
+++ 0_Fundamentals/object.js
@@ -0,0 +1,18 @@
+const prod1 = {}
+prod1.nome = "produto 1"
+prod1.preco = 899.90
+
+console.log(prod1)
+
+const prod2 = {
+    nome: "produto 2",
+    preco: 199.90,
+    obg: {
+        teste: "testando"
+    }
+}
+
+//json
+// '{ "nome": "produto 2", "preco": 199.90 }'
+
+console.log(prod2)
