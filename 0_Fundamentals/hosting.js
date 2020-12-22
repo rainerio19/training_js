b911097cb7fbcd32142046c091ab32856b02e4ef
rainerio <rai.inerio@hotmail.com>
2020-12-21 23:22:29 -0300

    hosting

---
+++ 0_Fundamentals/hosting.js
@@ -0,0 +1,11 @@
+console.log("i =", a);
+var a = 3;
+console.log("i = ", a);
+
+console.log(typeof Object);
+console.log(typeof new Object());
+
+class Produto {}
+
+console.log(typeof Produto);
+console.log(typeof new Produto());
