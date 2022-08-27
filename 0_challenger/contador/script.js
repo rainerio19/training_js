5d049d55217e19c30ae51bc8e1125c7992b90fe8
rainerio19 <rai.inerio@hotmail.com>
2022-08-26 23:41:04 -0300

    add prettier

--- 0_challenger/contador/script.js
+++ 0_challenger/contador/script.js
@@ -1,31 +1,30 @@
+const ADD = document.getElementsByName("adicionar")[0];
+const REMOVE = document.getElementsByName("subtrair")[0];
+const CURRENTNUMBER = document.getElementById("currentNumber");
+let cont = 0;
 
-  const ADD = document.getElementsByName('adicionar')[0];
-  const REMOVE = document.getElementsByName('subtrair')[0];
-  const CURRENTNUMBER = document.getElementById('currentNumber')
-  let cont = 0;
-  
-  ADD.addEventListener("click", increment);
-  REMOVE.addEventListener("click", decrement);
+ADD.addEventListener("click", increment);
+REMOVE.addEventListener("click", decrement);
 
-function increment(){
-  REMOVE.disabled=false
-  CURRENTNUMBER.style.color='black'
+function increment() {
+	REMOVE.disabled = false;
+	CURRENTNUMBER.style.color = "black";
 
-  cont++
-  CURRENTNUMBER.innerHTML=cont;
+	cont++;
+	CURRENTNUMBER.innerHTML = cont;
 
-  if(cont === 5) ADD.disabled=true
+	if (cont === 5) ADD.disabled = true;
 }
 
-function decrement(){
-  ADD.disabled=false
-  CURRENTNUMBER.style.color='black'
+function decrement() {
+	ADD.disabled = false;
+	CURRENTNUMBER.style.color = "black";
 
-  cont--
-  CURRENTNUMBER.innerHTML=cont
-  
-  if(cont === -1){
-    REMOVE.disabled=true
-    CURRENTNUMBER.style.color='red'
-  }
+	cont--;
+	CURRENTNUMBER.innerHTML = cont;
+
+	if (cont === -1) {
+		REMOVE.disabled = true;
+		CURRENTNUMBER.style.color = "red";
+	}
 }
